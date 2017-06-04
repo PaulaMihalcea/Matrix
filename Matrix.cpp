17d517e20b4d239c15e2f347ff26e1bd6e9b9f0f
Zelethil <mihalceapaula@gmail.com>
2017-06-04 16:42:12 +0200

    Added Matrix class

---
+++ Matrix.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Zelethil on 04.06.2017.
+//
+
+#include "Matrix.h"
---
+++ Matrix.h
@@ -0,0 +1,14 @@
+//
+// Created by Zelethil on 04.06.2017.
+//
+
+#ifndef UNTITLED_MATRIX_H
+#define UNTITLED_MATRIX_H
+
+
+class Matrix {
+
+};
+
+
+#endif //UNTITLED_MATRIX_H
