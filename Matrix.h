d534c5fcfa51cfe55d3dd7754b7260dbe5d2621b
Zelethil <mihalceapaula@gmail.com>
2017-06-18 12:25:44 +0200

    OVERHAUL (again)
    - Changed the base matrix to a one dimensional STL vector

--- Matrix.h
+++ Matrix.h
@@ -5,9 +5,12 @@
 #ifndef MATRIX_MATRIX_H
 #define MATRIX_MATRIX_H
 
-// TODO RES: https://www.programiz.com/cpp-programming/examples/matrix-multiplication
+// TODO Classi template per matrici con implementazione di operazioni come prodotto e trasposta, oltre che selezione di righe e colonne (usare Factory per creare gli oggetti)
+
+// TODO void functions (instead of boolean)
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -16,55 +19,56 @@ template<class T>
 class Matrix {
 public:
     Matrix(int r, int c, T v) : rows(r), cols(c) {
-        data = new int[r*c];
-        size = rows * cols;
-        for (int i=0; i<rows; i++)
-            for (int j=0; j<cols; j++)
-                setValue(i, j, v);
+        size = rows*cols;
+        for (int i=0; i<size; i++) {
+            data.push_back(v);
+        }
         cout << "Matrix created." << endl;
+        getMatrix();
     }
 
     ~Matrix() {
-        delete[] data;
+        data.erase(data.begin(), data.end());
+    }
+
+    int getRows() const {
+        return rows;
+    }
+
+    int getCols() const {
+        return cols;
     }
 
     bool setValue(int x, int y, T v){
-        if (x>=rows || y>=cols || x<0 || y<0){
+        if (x > rows || y > cols || x <= 0 || y <= 0){
             cout << "setValue: Invalid x or y" << endl;
             return false;
         }
-
         data[cols*(x-1)+(y-1)] = v;
-        cout << "Value at (" << x+1 << ", " << y+1 << ") set to " << v << "." << endl;
+        cout << "Value at (" << x << ", " << y << ") set to " << v << "." << endl;
         return true;
     }
 
-    int getValue(int x, int y, T& v) const {
-        if (x >= rows || y >= cols || x < 0 || y < 0) {
+    bool getValue(int x, int y, T& v) const {
+        if (x > rows || y > cols || x <= 0 || y <= 0) {
             cout << "getValue: Invalid x or y" << endl;
             return false;
         }
         v = data[cols*(x-1)+(y-1)];
-        cout << "Value at (" << x+1 << ", " << y+1 << "): " << v << "." << endl;
+        cout << "Value at (" << x << ", " << y << "): " << v << "." << endl;
         return true;
     }
 
     bool getRow(int x) const {
-        if (x > rows || x < 0) {
+        if (x > rows || x <= 0) {
             cout << "getRow: Invalid row number" << endl;
             return false;
         }
         T v;
-        for (int j=0; j<cols; j++) {
-            getValue(x-1, j, v);
+        cout << "Row " << x << ": ";
+        for (int j=1; j<=cols; j++) {
+            getValue_(x, j, v);
         }
-        /*row = new int[cols];
-        for (int j=0; j<cols; j++) {
-            int value = cols * (x - 1) + j;
-            cout << "Value: " << value << endl;
-            row[j] = data[value];
-            cout << "Column " << j+1 << ": " << row[j] << endl;
-        }*/
         return true;
     }
 
@@ -81,15 +85,15 @@ public:
     }
 
     bool getMatrix() const {
-        int i;
-        for (i=1; i<=rows; i++) {
+        cout << "Matrix:" << endl;
+        for (int i=1; i<=rows; i++) {
             getRow_(i);
             cout << endl;
         }
     }
 
-    int getValue_(int x, int y, T& v) const {
-        if (x >= rows || y >= cols || x < 0 || y < 0) {
+    bool getValue_(int x, int y, T& v) const {
+        if (x > rows || y > cols || x <= 0 || y <= 0) {
             cout << "getValue: Invalid x or y" << endl;
             return false;
         }
@@ -99,29 +103,33 @@ public:
     }
 
     bool getRow_(int x) const {
-        if (x > rows || x < 0) {
+        if (x > rows || x <= 0) {
             cout << "getRow: Invalid row number" << endl;
             return false;
         }
         T v;
-        for (int j=0; j<cols; j++) {
-            getValue_(x-1, j, v);
+        for (int j=1; j<=cols; j++) {
+            getValue_(x, j, v);
         }
-        /*row = new int[cols];
-        for (int j=0; j<cols; j++) {
-            int value = cols * (x - 1) + j;
-            cout << "Value: " << value << endl;
-            row[j] = data[value];
-            cout << "Column " << j+1 << ": " << row[j] << endl;
-        }*/
         return true;
+    }
+
+    Matrix<T> transpose() const {
+        Matrix<T> tr(rows, cols, 0);
+        for (int i=1; i<=rows; i++)
+            for (int j=1; j<=cols; j++) {
+                T v;
+                //this->getValue(i, j, v);
+                //tr.setValue(i, j, v);
+            }
+        return tr;
     }
 
 private:
     const int rows;
     const int cols;
     int size;
-    T* data;
+    vector<T> data;
 };
 
 #endif //MATRIX_MATRIX_H
--- main.cpp
+++ main.cpp
@@ -3,42 +3,34 @@
 
 int main() {
 
-    Matrix<int> m(3, 3, 0);
+    Matrix<int> m(4, 3, 0);
+    int v;
+    m.setValue(1, 1, 1);
+    m.setValue(1, 2, 2);
+    m.setValue(1, 3, 3);
+    m.setValue(2, 1, 5);
+    m.setValue(2, 2, 6);
+    m.setValue(2, 3, 7);
+    m.setValue(3, 1, 9);
+    m.setValue(3, 2, 10);
+    m.setValue(3, 3, 11);
+    m.setValue(4, 1, 13);
+    m.setValue(4, 2, 14);
+    m.setValue(4, 3, 15);
 
-    m.setValue(0, 0, 1);
-    m.setValue(0, 1, 2);
+    cout << "-------" << endl;
+
+    m.getRow(1);
+    m.getMatrix();
+
+    //m.setValue(0, 0, 1);
+    //m.getMatrix();
+    /*m.setValue(0, 1, 2);
     //m.setValue(0, 2, 3);
     //m.setValue(0, 3, 4);
     m.setValue(1, 0, 5);
     m.setValue(1, 1, 6);
-    /*m.setValue(1, 2, 7);
-    m.setValue(1, 3, 8);
-    m.setValue(2, 0, 9);
-    m.setValue(2, 1, 10);
-    m.setValue(2, 2, 11);
-    m.setValue(2, 3, 12);
-    m.setValue(3, 0, 13);
-    m.setValue(3, 1, 14);
-    m.setValue(3, 2, 15);
-    m.setValue(3, 3, 16);*/
-    int v;
-    //m.getValue(1, 1, v);
-
-    //int *row;
-    //m.getRow(3);
-    //m.getCol(3);
-    m.getMatrix();
-    //cout << m.getCols() << endl;
-
-    /*Matrix<int> sm2(5, 4);
-    int value2 = 1;
-    sm2.getValue(2, 3, value2);
-    cout << "value2 should be 0: " << value2 << endl;
-
-    Matrix<int> sm3(5, 4);
-    sm3.setValue(2, 3, 5);
-    int value3 = 0;
-    sm3.getValue(2, 3, value3);*/
+    m.getMatrix();*/
 
     return 0;
 }
